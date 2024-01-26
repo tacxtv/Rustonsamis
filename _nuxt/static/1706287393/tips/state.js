f74480673517eb708fb0bb553fc6a745a49c2fd1
tacxou <tacxou@users.noreply.github.com>
2024-01-26 17:00:03 +0000

    deploy: 3c7bc66c86ad00d59391316cdafb12267c09f26a

--- _nuxt/static/1706287393/tips/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Frustonsamis\u002F_nuxt\u002Fstatic\u002F1706287393",layout:"default",error:a,state:{auth:{user:a,loggedIn:false,strategy:""}},serverRendered:true,routePath:"\u002Ftips",config:{_app:{basePath:"\u002Frustonsamis\u002F",assetsPath:"\u002Frustonsamis\u002F_nuxt\u002F",cdnURL:a}}}}(null));
